50d87033d682caf54ee85620f5b9cd9418e9282c
Edward A Maxedon <edadma@gmail.com>
2018-09-17 01:05:04 -0400

    corrections

--- test/startup.s
+++ test/startup.s
@@ -6,26 +6,26 @@
   .text
 _start:
   /* zero bss section */
-  movea.l   #__bss_start__, a0
-  movea.l   #__bss_end__, a1
+  movea.l   #__bss_start__, %a0
+  movea.l   #__bss_end__, %a1
 .1:
-  cmpa.l	  a0, a1
+  cmpa.l    %a0, %a1
   jeq       .2
-  move.b    #0, a0@+
+  move.b    #0, %a0@+
   jmp       .1
 .2:
   /* copy data section */
-  movea.l   #__data_rom_start__, a0
-  movea.l   #__data_start__, a1
-  movea.l   #__data_end__, a2
+  movea.l   #__data_rom_start__, %a0
+  movea.l   #__data_start__, %a1
+  movea.l   #__data_end__, %a2
 .3:
-  cmpa.l	  a2, a1
+  cmpa.l    %a2, %a1
   jeq       .4
-  move.b    a0@+, a1@+
+  move.b    %a0@+, %a1@+
   jmp       .3
-.4
+.4:
   /* jsr     main */
-  jmp     halt
+  jmp       halt
 
   .globl	outc
   .type	outc, @function
