ca1eff03ee36002a2b12cdd656336501bbebcad2
Edward A Maxedon, Sr <edadma@gmail.com>
2018-10-02 14:22:55 -0400

    code debug info reader

---
+++ src/main/scala/DebugFileReader.scala
@@ -0,0 +1,24 @@
+package xyz.hyperreal.m68k
+
+import scala.collection.mutable.HashMap
+
+
+object DebugFileReader extends App {
+
+  println( apply(io.Source.fromFile("tools/main.debug")) )
+
+  lazy val lineRegex = """ */\* file ([^ ]+) line ([^ ]+) addr 0x([^ ]+) \*/"""r
+
+  def apply( src: io.Source ): HashMap[Long, (String, String)] = {
+    val code = new HashMap[Long, (String, String)]
+
+    for (line <- src.getLines)
+      line match {
+        case lineRegex( file, lineno, address ) => code(java.lang.Long.parseLong( address, 16 )) = (lineno, file)
+        case _ =>
+      }
+
+    code
+  }
+
+}
